b90bd77d60b191210f81621fac63f459d544485a
Innokentii Smirnov <innokenty.smirnoff@gmail.com>
2026-06-20 10:52:20 +0200

    Use additional strict type checking options (all set by --strict except for --disallow-untyped-calls

--- typecheck.sh
+++ typecheck.sh
@@ -2,4 +2,16 @@ clear
 MYPYPATH=packages mypy applications/select_lemma_by_log_probs.py \
   --follow-untyped-imports \
   --disallow-untyped-defs \
-  --disallow-any-generics
+  --disallow-any-generics \
+  --warn-unused-configs \
+  --disallow-subclassing-any \
+  --disallow-incomplete-defs \
+  --check-untyped-defs \
+  --disallow-untyped-decorators \
+  --warn-redundant-casts \
+  --warn-unused-ignores \
+  --warn-return-any \
+  --no-implicit-reexport \
+  --strict-equality \
+  --strict-bytes \
+  --extra-checks
